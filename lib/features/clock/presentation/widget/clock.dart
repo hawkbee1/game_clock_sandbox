97dcb876b16f903ab8d636ac5afec88afc51936b
hawkbee <googandads@gmail.com>
2019-11-24 11:29:19 +0100

    [green] widget with CLOCK key is displayed

--- lib/features/clock/presentation/widget/clock.dart
+++ lib/features/clock/presentation/widget/clock.dart
@@ -1,10 +1,12 @@
 
 import 'package:flutter/material.dart';
+import 'package:game_clock/core/strings/widgets_keys.dart';
 
 class Clock extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      key: Key(CLOCK),
       child: Text('This is a clock'),
     );
   }
---
+++ test/features/clock/presentation/pages/clock_page_test.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:game_clock/core/strings/widgets_keys.dart';
+import 'package:game_clock/features/clock/presentation/pages/clock_page.dart';
+
+void main() {
+
+  group('test login page is correctly displayed', () {
+
+    testWidgets('check label elements of login page are here', (WidgetTester tester) async {
+      await tester.pumpWidget(MyClockPage());
+      final clockFinder = find.byKey(Key(CLOCK));
+
+      expect(clockFinder, findsOneWidget);
+
+    });
+  });
+
+}
+
+class MyClockPage extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'Where Assistant',
+      home: ClockPage(title: 'toto',),);
+  }
+}
