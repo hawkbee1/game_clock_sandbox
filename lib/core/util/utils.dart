b34cc21c1da6bad0624597aef15acb0c462a8bc7
hawkbee <googandads@gmail.com>
2019-11-27 09:37:40 +0100

    display a clock with hours, minutes, seconds

---
+++ lib/core/util/utils.dart
@@ -0,0 +1,9 @@
+/// Returns a human-readable representation of a duration
+String prettyPrintDuration(Duration duration, [showSeparator = true]) {
+  final hours = duration.inHours % 24;
+  final minutes = duration.inMinutes % 60;
+  final seconds = duration.inSeconds % 60;
+  final separator = showSeparator ? ':' : ' ';
+  return '$hours$separator${prettyPrintDigits(minutes)}$separator${prettyPrintDigits(seconds)}';
+}
+String prettyPrintDigits(int num) => num >= 10 ? '$num' : '0$num';
--- lib/features/clock/presentation/widget/clock.dart
+++ lib/features/clock/presentation/widget/clock.dart
@@ -1,13 +1,24 @@
-
 import 'package:flutter/material.dart';
 import 'package:game_clock/core/strings/widgets_keys.dart';
+import 'package:game_clock/core/util/utils.dart';
 
 class Clock extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
       key: Key(CLOCK),
-      child: Text('This is a clock'),
+      child: Text(
+        prettyPrintDuration(Duration(
+          hours: 1,
+          minutes: 32,
+          seconds: 13,
+        )),
+        style: TextStyle(
+          fontWeight: FontWeight.bold,
+          fontSize: 60,
+          color: Colors.black,
+        ),
+      ),
     );
   }
 }
