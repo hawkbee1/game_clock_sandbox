548fe5bf97edb0dc673684f735e4e74bf657e80a
hawkbee <googandads@gmail.com>
2019-12-02 10:58:19 +0100

    change active player when removed is pressed and active player is the last one
    we don't remove the first player if is alone

--- lib/features/clock/domain/usecases/remove_player.dart
+++ lib/features/clock/domain/usecases/remove_player.dart
@@ -1,13 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:game_clock/features/clock/domain/entities/player.dart';
+import 'package:game_clock/features/clock/domain/repositories/active_player_repository.dart';
 import 'package:game_clock/features/clock/domain/repositories/player_list.dart';
 import 'package:game_clock/injection_container.dart';
 
 class RemovePlayerButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    PlayerList playerList = sl();
+    PlayerList _playerList = sl();
+    ActivePlayer _activePlayer = sl();
     return FloatingActionButton(
-      onPressed: () => playerList.removePlayer(),
+      onPressed: () {
+        debugPrint('length ${_playerList.players.length}');
+        if(_activePlayer.player.playerId == _playerList.players.elementAt(_playerList.players.length-1).playerId) {
+          _activePlayer.nextPlayer();
+        }
+        if(_playerList.players.length > 1) _playerList.removePlayer();
+      },
       tooltip: 'Remove last player',
       child: IconButton(icon: Icon(Icons.remove), onPressed: null),);
   }
