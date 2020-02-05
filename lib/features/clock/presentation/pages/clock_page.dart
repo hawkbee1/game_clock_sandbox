f1580096e86a0b20e4b986db4cf9292430752243
hawkbee <googandads@gmail.com>
2020-02-05 18:38:38 +0100

    Extract player list and its buttons in separate file: player_list.dart

--- lib/features/clock/presentation/pages/clock_page.dart
+++ lib/features/clock/presentation/pages/clock_page.dart
@@ -1,20 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
-import 'package:flutter_game_clock/features/clock/domain/usecases/add_player.dart';
-import 'package:flutter_game_clock/features/clock/domain/usecases/remove_player.dart';
 import 'package:flutter_game_clock/features/clock/presentation/widget/global_clock.dart';
-import 'package:flutter_game_clock/features/clock/presentation/widget/nb_players.dart';
 import 'package:flutter_game_clock/features/clock/presentation/widget/player_clock.dart';
+import 'package:flutter_game_clock/features/clock/presentation/widget/players_list.dart';
 
 class ClockPage extends StatelessWidget {
   ClockPage();
 
   @override
   Widget build(BuildContext context) {
-    bool _isPortrait =
-        MediaQuery.of(context).orientation == Orientation.portrait
-            ? true
-            : false;
     return Scaffold(
       body: Container(
         decoration: BoxDecoration(
@@ -24,36 +18,15 @@ class ClockPage extends StatelessWidget {
         )),
         child: Center(
           child: Flex(
-            direction: _isPortrait ? Axis.vertical : Axis.horizontal,
+            direction: Axis.vertical,
             children: <Widget>[
               Spacer(),
               GlobalClock(),
+              Spacer(),
+              PlayersList(),
+              Spacer(),
               Center(child: PlayerClock()),
               Spacer(),
-              Flex(
-                direction: _isPortrait ? Axis.horizontal : Axis.vertical,
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: <Widget>[
-                  Container(
-                    child: NbPlayers(),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(bottom: 8.0),
-                  child: Row(
-                    children: <Widget>[
-                      Padding(
-                        padding: const EdgeInsets.all(16.0),
-                        child: RemovePlayerButton(),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.all(16.0),
-                        child: AddPlayerButton(),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
             ],
           ),
         ),
