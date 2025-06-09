cd821941ce7afc14c3c743ec8f7d4d711603bcf1
hawkbee1 <googandads@gmail.com>
2025-06-09 15:37:29 +0000

    Refactor Game Clock application structure and UI components
    That's a mobile utiliy project in flutter. This app track time of a game and the playing time of each player. From the pasted image create the app with following functionalities:
    
    add/remove players
    start game stopwatch which also start first player stopwatch
    pause all stopwatches
    reset all stopwatches
    On the left the game stopwatch is displayed
    on the center a big button displays the player number and his own stopwatch
    When user press the big button current player stopwatch is paused and next player stopwatch and number is displayed and started
    Workspace
    used /new (rerun without)
    Sure, here's a proposed directory structure for
    
    Generating workspace preview...
    
    That's a mobile utiliy project in flutter. This app track time of a game and the playing time of each player. From the pasted image create the app with following functionalities:
    
    add/remove players
    start game stopwatch which also start first player stopwatch
    pause all stopwatches
    reset all stopwatches
    On the left the game stopwatch is displayed
    on the center a big button displays the player number and his own stopwatch
    When user press the big button current player stopwatch is paused and next player stopwatch and number is displayed and started
    
    GitHub Copilot
    I'll help you create this Flutter game clock app. First, let's read the current content of the main
    
    That's a mobile utiliy project in flutter. This app track time of a game and the playing time of each player. From the pasted image create the app with following functionalities:
    
    add/remove players
    start game stopwatch which also start first player stopwatch
    pause all stopwatches
    reset all stopwatches
    On the left the game stopwatch is displayed
    on the center a big button displays the player number and his own stopwatch
    When user press the big button current player stopwatch is paused and next player stopwatch and number is displayed and started

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
 
 void main() {
   runApp(const MyApp());
@@ -7,116 +8,248 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Game Clock',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+        useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const GameClockPage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class GameClockPage extends StatefulWidget {
+  const GameClockPage({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<GameClockPage> createState() => _GameClockPageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _GameClockPageState extends State<GameClockPage> {
+  final List<Stopwatch> _playerStopwatches = [];
+  final Stopwatch _gameStopwatch = Stopwatch();
+  Timer? _timer;
+  int _currentPlayerIndex = 0;
+  int _numberOfPlayers = 5;
+  bool _isGameRunning = false;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+  @override
+  void initState() {
+    super.initState();
+    _initializeStopwatches();
+    _startTimer();
+  }
+
+  void _initializeStopwatches() {
+    _playerStopwatches.clear();
+    for (int i = 0; i < _numberOfPlayers; i++) {
+      _playerStopwatches.add(Stopwatch());
+    }
+  }
+
+  void _startTimer() {
+    _timer = Timer.periodic(const Duration(milliseconds: 10), (timer) {
+      if (mounted) setState(() {});
     });
+  }
+
+  String _formatDuration(Duration duration) {
+    String twoDigits(int n) => n.toString().padLeft(2, '0');
+    String minutes = twoDigits(duration.inMinutes.remainder(60));
+    String seconds = twoDigits(duration.inSeconds.remainder(60));
+    String centiseconds = twoDigits(
+      duration.inMilliseconds.remainder(1000) ~/ 10,
+    );
+    return "$minutes:$seconds:$centiseconds";
+  }
+
+  void _startGame() {
+    setState(() {
+      _isGameRunning = true;
+      _gameStopwatch.start();
+      _playerStopwatches[_currentPlayerIndex].start();
+    });
+  }
+
+  void _pauseGame() {
+    setState(() {
+      _isGameRunning = false;
+      _gameStopwatch.stop();
+      _playerStopwatches[_currentPlayerIndex].stop();
+    });
+  }
+
+  void _resetGame() {
+    setState(() {
+      _isGameRunning = false;
+      _gameStopwatch.stop();
+      _gameStopwatch.reset();
+      for (var stopwatch in _playerStopwatches) {
+        stopwatch.stop();
+        stopwatch.reset();
+      }
+      _currentPlayerIndex = 0;
+    });
+  }
+
+  void _nextPlayer() {
+    if (!_isGameRunning) return;
+    setState(() {
+      _playerStopwatches[_currentPlayerIndex].stop();
+      _currentPlayerIndex = (_currentPlayerIndex + 1) % _numberOfPlayers;
+      _playerStopwatches[_currentPlayerIndex].start();
+    });
+  }
+
+  void _addPlayer() {
+    if (_numberOfPlayers < 10) {
+      setState(() {
+        _numberOfPlayers++;
+        _playerStopwatches.add(Stopwatch());
+      });
+    }
+  }
+
+  void _removePlayer() {
+    if (_numberOfPlayers > 2) {
+      setState(() {
+        _numberOfPlayers--;
+        _playerStopwatches.removeLast();
+        if (_currentPlayerIndex >= _numberOfPlayers) {
+          _currentPlayerIndex = _numberOfPlayers - 1;
+        }
+      });
+    }
+  }
+
+  @override
+  void dispose() {
+    _timer?.cancel();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+        backgroundColor: Colors.blue,
+        title: const Text(
+          'Clock for your games',
+          style: TextStyle(color: Colors.white),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      body: Column(
+        children: [
+          Expanded(
+            child: Row(
+              children: [
+                // Game stopwatch
+                Expanded(
+                  flex: 2,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        _formatDuration(_gameStopwatch.elapsed),
+                        style: const TextStyle(
+                          fontSize: 48,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          IconButton(
+                            icon: Icon(
+                              _isGameRunning ? Icons.pause : Icons.play_arrow,
+                            ),
+                            onPressed: _isGameRunning ? _pauseGame : _startGame,
+                            color: Colors.blue,
+                            iconSize: 48,
+                          ),
+                          IconButton(
+                            icon: const Icon(Icons.stop),
+                            onPressed: _resetGame,
+                            color: Colors.red,
+                            iconSize: 48,
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+                // Player display
+                Expanded(
+                  flex: 3,
+                  child: GestureDetector(
+                    onTap: _nextPlayer,
+                    child: Container(
+                      margin: const EdgeInsets.all(16),
+                      padding: const EdgeInsets.all(16),
+                      decoration: BoxDecoration(
+                        color: Colors.lightBlue.shade100,
+                        borderRadius: BorderRadius.circular(16),
+                      ),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            'Player: ${_currentPlayerIndex + 1}',
+                            style: const TextStyle(fontSize: 36),
+                          ),
+                          const SizedBox(height: 8),
+                          Text(
+                            _formatDuration(
+                              _playerStopwatches[_currentPlayerIndex].elapsed,
+                            ),
+                            style: const TextStyle(
+                              fontSize: 48,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                // Player controls
+                Expanded(
+                  flex: 1,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        'Number of players: $_numberOfPlayers',
+                        textAlign: TextAlign.center,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          IconButton(
+                            icon: const Icon(Icons.remove),
+                            onPressed: _removePlayer,
+                            color: Colors.blue,
+                            iconSize: 32,
+                          ),
+                          IconButton(
+                            icon: const Icon(Icons.add),
+                            onPressed: _addPlayer,
+                            color: Colors.blue,
+                            iconSize: 32,
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
