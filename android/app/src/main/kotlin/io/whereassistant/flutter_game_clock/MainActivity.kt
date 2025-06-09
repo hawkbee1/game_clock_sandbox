bb68d56ad45aad5a72310e2a1a2e1cce80218c83
hawkbee1 <googandads@gmail.com>
2025-06-09 14:29:51 +0000

    rm -R *

--- android/app/src/main/kotlin/io/whereassistant/flutter_game_clock/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package io.whereassistant.flutter_game_clock
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- lib/core/themes/styles.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-
-const bigTextStyle = TextStyle(
-  fontWeight: FontWeight.bold,
-  fontSize: 60,
-  color: Colors.black,
-);
