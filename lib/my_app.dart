6c6fd9e857c7dc920dc1b765355ce349d00de044
hawkbee1 <googandads@gmail.com>
2026-04-09 11:48:22 +0000

    change Application name to "Turn Tracker Pro"

--- lib/my_app.dart
+++ lib/my_app.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Game Clock',
+      title: 'Turn Tracker Pro',
       theme: AppTheme.light,
       home: const GamePage(),
     );
